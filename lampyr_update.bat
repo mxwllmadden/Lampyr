55bfac430ee0f8e0e9d783c5aeae2301121fd194
Maxwell Madden <135063494+mxwllmadden@users.noreply.github.com>
2026-03-12 20:11:52 -0400

    Use call for conda activation in batch file
    Prefix conda activate commands with `call` in lampyr_update.bat so the batch script continues executing after invoking the conda activation batch. This prevents premature termination when activating the base and mx_hardware environments before uninstalling/installing the lampyr package.

--- lampyr_update.bat
+++ lampyr_update.bat
@@ -1,7 +1,7 @@
 @echo
 
-conda activate
-conda activate mx_hardware
+call conda activate
+call conda activate mx_hardware
 pip uninstall lampyr -y
 pip install "git+https://github.com/mxwllmadden/Lampyr.git@main"
 cmd /k
